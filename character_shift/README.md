660040cb8db7e431ac0ac9fa033e31c8d7a9ea48
Martijn Arts <arts.martijn@gmail.com>
2015-04-15 15:32:50 +0200

    Add first challenge.

---
+++ character_shift/README.md
@@ -0,0 +1,12 @@
+# Character Shift
+
+Performs a caesar shift on the string.
+
+## Parameters
+
+* `string`  
+   The string to be shifted. (str)
+* `key`  
+   The number of places to rotate the string by. (int)
+*  `decipher`  
+   Set to True to decipher. (bool)
---
+++ character_shift/character_shift.py
@@ -0,0 +1,9 @@
+def shift(string, key, decipher=False):
+    return ''.join(
+        chr((ord(c.upper())-65+key*(1-2*decipher)) % 26+65+32*c.islower())
+        if c.isalpha() else c for c in string)
+
+if __name__ == '__main__':
+    assert shift('abcz+', 1) == 'bcda+', shift('abcz+', 1)
+    assert shift('ABCZ+', 1) == 'BCDA+', shift('ABCZ+', 1)
+    assert shift('bcda+', 1, True) == 'abcz+', shift('bcda+', 1, True)
