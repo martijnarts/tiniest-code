dc02a3cb637c54e6f51bbffc0694d2610e598c1c
Stephen Swatman <stephenswat@gmail.com>
2015-04-16 01:41:03 +0200

    Further reduce code size by using modulo operators
    a & 31 is essentially equivalent to a % 32, but the latter allows
    us to remove parentheses because of higher operator binding. Also,
    use '-1' instead of '+25' to correct for modulo calculations as
    they are equivalent but '-1' saves a single digit.

--- character_shift/character_shift.py
+++ character_shift/character_shift.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def shift(string, key, decipher=False):
     return ''.join(
-        chr((ord(c) & 224)+((ord(c) & 31)+25+key*(-2*decipher+1)*c.isalpha())
-            % 26+1) for c in string)
+        chr((ord(c) & 224)+(ord(c) % 32-1+key*(-2*decipher+1)*c.isalpha()) %
+            26+1) for c in string)
 
 if __name__ == '__main__':
     assert shift('abcz+', 1) == 'bcda+', shift('abcz+', 1)
