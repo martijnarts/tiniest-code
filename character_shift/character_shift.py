72980ca43eeb2f3e4ea22553afe07f71eede4906
Stephen Swatman <stephenswat@gmail.com>
2015-04-15 23:08:40 +0200

    10 characters shorter

--- character_shift/character_shift.py
+++ character_shift/character_shift.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def shift(string, key, decipher=False):
     return ''.join(
-        chr((ord(c.upper())-65+key*(1-2*decipher)) % 26+65+32*c.islower())
-        if c.isalpha() else c for c in string)
+        [chr((ord(x) & 224)+((ord(x) & 31)-2*decipher) % 26+1)
+         if ord(x) & 64 else x for x in string])
 
 if __name__ == '__main__':
     assert shift('abcz+', 1) == 'bcda+', shift('abcz+', 1)
