14561c4bcac0372d407163e91979209f2551a7a2
Stephen Swatman <stephenswat@gmail.com>
2015-04-15 23:21:14 +0200

    Actually, this is even shorter

--- character_shift/character_shift.py
+++ character_shift/character_shift.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def shift(string, key, decipher=False):
     return ''.join(
-        [chr((ord(x) & 224)+((ord(x) & 31)-2*decipher) % 26+1)
-         if ord(x) & 64 else x for x in string])
+        chr((ord(x) & 224)+((ord(x) & 31)-2*decipher) % 26+1)
+        if ord(x) & 64 else x for x in string)
 
 if __name__ == '__main__':
     assert shift('abcz+', 1) == 'bcda+', shift('abcz+', 1)
