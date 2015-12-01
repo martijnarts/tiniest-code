b9cd6033eab3df457224c050a9d3d8e76a871da5
Bob <iam@willdover.co.uk>
2015-12-01 00:45:04 +0000

    PEP-8, docs

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,5 +1,7 @@
 def bubble_sort(ls):
-    for i in range(len(ls)-1):
-        if ls[i] > ls[i+1]:
-            ls[i], ls[i+1] = ls[i+1], ls[i]
-    return bubble_sort(ls) if any([ls[i] > ls[i+1] for i in range(len(ls)-1)]) else ls
+    for i in range(len(ls) - 1):
+        if ls[i] > ls[i + 1]:
+            ls[i], ls[i + 1] = ls[i + 1], ls[i]
+    return bubble_sort(ls) if any(
+                              [ls[i] > ls[i + 1] for i in range(len(ls) - 1)]
+                                  ) else ls
