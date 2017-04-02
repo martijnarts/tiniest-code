709bd06c9506f5e43b64d3932b39694b7d2415fd
Martijn Arts <arts.martijn@gmail.com>
2017-04-02 20:59:55 +0200

    Fix character shift Flake8

--- character_shift/character_shift.py
+++ character_shift/character_shift.py
@@ -4,6 +4,7 @@ def shift(string, key, decipher=False):
         chr((ord(c) & 224)+(ord(c) % 32-1-key*(2*decipher-1)*c.isalpha()) %
             26+1) for c in string)
 
+
 if __name__ == '__main__':
     assert shift('abcz+', 1) == 'bcda+', shift('abcz+', 1)
     assert shift('ABCZ+', 1) == 'BCDA+', shift('ABCZ+', 1)
